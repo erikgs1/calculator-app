0804eb040d00ffe41dca3718b1997e39cce85e55
erikgs1 <erik.contato15@gmail.com>
2020-05-20 20:13:04 -0300

    fiz a calculadora funcionar

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 var buttons = document.querySelectorAll(".buttons");
 var operator = document.querySelectorAll(".operator");
-
+var equal = document.querySelector(".equal");
+var operations = {
+    '+': function (param1, param2) {return param1 + param2;},
+    '-': function (param1, param2) {return param1 - param2;},
+    '*': function (param1, param2) {return param1 * param2;},
+    '/': function (param1, param2) {return param1 / param2;},
+    '%': function (param1, param2) {return param1 % param2;}
+}
 for (var i = 0; i < buttons.length; i++ ){
     buttons[i].onclick = function(){
         if(document.getElementById('result1').innerHTML === ''){
@@ -24,7 +31,38 @@ for (var j = 0; j < operator.length; j++ ){
         operation(this.innerHTML);
     };
 }
-
 function operation(ope){
     document.getElementById('operator').innerHTML = ope;
+
 }
+equal.onclick = function(){
+    var op = document.getElementById('operator').innerHTML;
+    var soma = Number(document.getElementById('result1').innerHTML) + Number(document.getElementById('result2').innerHTML);
+    var subtration = Number(document.getElementById('result1').innerHTML) - Number(document.getElementById('result2').innerHTML);
+    var multiplication = Number(document.getElementById('result1').innerHTML) * Number(document.getElementById('result2').innerHTML);    
+    var divison = Number(document.getElementById('result1').innerHTML) / Number(document.getElementById('result2').innerHTML);
+    var mod = Number(document.getElementById('result1').innerHTML) % Number(document.getElementById('result2').innerHTML);
+    if(op === '+'){
+        document.getElementById('result').innerHTML = soma;
+    }
+    else if(op === '-'){
+        document.getElementById('result').innerHTML = subtration;
+    }
+    else if(op === 'x'){
+        document.getElementById('result').innerHTML = multiplication;
+    }
+    else if(op === '&#247;'){
+        document.getElementById('result').innerHTML = divison;
+    }
+    else{
+        document.getElementById('result').innerHTML = mod;
+    }
+}
+// function calculator(e) {
+//     return function(x, y){
+//         if (typeof x !== 'number' || typeof y !== 'number'){
+//             return false;
+//         }
+//     return operation[operator] (x, y);
+//     }
+// }

890ab73671a2d4e183c6a8de2307cad535f6b668
erikgs1 <erik.contato15@gmail.com>
2020-05-20 15:21:27 -0300

    adicionei o segundo numero

--- script.js
+++ script.js
@@ -1,12 +1,30 @@
 var buttons = document.querySelectorAll(".buttons");
-var num1 = 0;
-var num2 = 0;
-var ope = '';
+var operator = document.querySelectorAll(".operator");
+
 for (var i = 0; i < buttons.length; i++ ){
     buttons[i].onclick = function(){
-        firstNumber(this.innerHTML);
+        if(document.getElementById('result1').innerHTML === ''){
+            firstNumber(this.innerHTML);
+        }
+        else {
+            secondNumber(this.innerHTML);
+        }
     };
 }
 function firstNumber(num){
     document.getElementById('result1').innerHTML = num;
+}
+function secondNumber(num2){
+    document.getElementById('result2').innerHTML = num2;
+}
+//
+
+for (var j = 0; j < operator.length; j++ ){
+    operator[j].onclick = function(){
+        operation(this.innerHTML);
+    };
+}
+
+function operation(ope){
+    document.getElementById('operator').innerHTML = ope;
 }
